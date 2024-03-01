111285ccb6036916515c96f2f91a82b642bd6ad1
Lazar Jeremic <161849139+Dzerema@users.noreply.github.com>
2024-03-01 20:12:30 +0100

    Update custom.js
    navbar -> bottom-border i sklanjanje box-shadowa

--- PROJEKAT/JS/custom.js
+++ PROJEKAT/JS/custom.js
@@ -1,6 +1,19 @@
 function hover(){
-    document.getElementById("mini").src = "images/balkan_crni.png"
+    document.getElementById("mini").src = "/images/balkan_crni.png"
 }
 function unhover(){
-    document.getElementById("mini").src = "images/balkan.png"
+    document.getElementById("mini").src = "/images/balkan.png"
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 499 || document.documentElement.scrollTop > 499) {
+    document.getElementById("nav").style.boxShadow = "none";
+    document.getElementById("nav").style.borderBottom = "1px solid #fbcc01";
+  } 
+  else {
+    document.getElementById("nav").style.boxShadow = "0px 6px 30px 7px #000000";
+    document.getElementById("nav").style.borderBottom = "none";
+  }
 }
