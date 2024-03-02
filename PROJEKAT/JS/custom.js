508b56ba09d0b937cca1181d95d720a77723b467
Lazar Jeremic <161849139+Dzerema@users.noreply.github.com>
2024-03-02 19:35:26 +0100

    Update custom.js

--- PROJEKAT/JS/custom.js
+++ PROJEKAT/JS/custom.js
@@ -12,8 +12,16 @@ function scrollFunction() {
     document.getElementById("nav").style.boxShadow = "none";
     document.getElementById("nav").style.borderBottom = "1px solid #fbcc01";
   } 
-  else {
+  else if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
     document.getElementById("nav").style.boxShadow = "0px 6px 30px 7px #000000";
     document.getElementById("nav").style.borderBottom = "none";
+    document.getElementById("nav").style.backdropFilter = "blur(2px)";
+    document.getElementById("nav").style.backgroundColor = "rgba(1,1,1,0.3)";
+  }
+  else {
+    document.getElementById("nav").style.boxShadow = "none";
+    document.getElementById("nav").style.borderBottom = "none";
+    document.getElementById("nav").style.backdropFilter = "none";
+    document.getElementById("nav").style.backgroundColor = "rgba(1,1,1,0)";
   }
 }
